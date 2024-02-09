bd88c01b2dbd815900636829f24568838080f588
Kanishka23singh <92868964+Kanishka23singh@users.noreply.github.com>
2024-02-09 22:17:29 +0530

    Update 08.Longest Substring  With No Repeating Characters.java

--- Sliding Window/08.Longest Substring  With No Repeating Characters.java
+++ Sliding Window/08.Longest Substring  With No Repeating Characters.java
@@ -1,6 +1,5 @@
 // See Leetcode 3 also : Length of longest Substring Without Repeating Characters
-/*It is same as Longest substring with K Unique Characters but here we want all unique characters. Therefore here k is window size (j - i + 1). 
-But a bit different approach to refer see https://www.youtube.com/watch?v=qtVh-XEpsJo*/
+// A bit different approach to refer see https://www.youtube.com/watch?v=qtVh-XEpsJo//
 
 import java.util.HashMap;
 class Main {
@@ -8,27 +7,42 @@ class Main {
         HashMap<Character, Integer> map = new HashMap<>();
         int i = 0, j = 0;
         int max = 0;
-        int start = 0, end = 0;
-        while (j < s.length()) {
-            if (map.containsKey(s.charAt(j))) {
+        
+        while(j < s.length()){
+            if(map.containsKey(s.charAt(j))){
                 i = Math.max(i, map.get(s.charAt(j)) + 1);
             }
-            map.put(s.charAt(j), j);
-            if (j - i + 1 > max) {
-                max = j - i + 1;
-                start = i;
-                end = j + 1;
-            }
+            map.put(s.charAt(j),j);
+            max = Math.max(max, j - i + 1);
             j++;
         }
-        return s.substring(start, end);
-    }
-
-    public static void main(String[] args) {
-        // Test the longestSubstrDistinctChars method
-        String s =  "geeksforgeeks";
-        System.out.println("Longest substring with distinct characters: " + longestSubstrDistinctChars(s));
+        return max;
     }
 }
 
+// Same approach as Length of Longest substring with K Unique Characters but here we want all unique characters. Therefore here k is window size (j - i + 1). 
+class Solution{
+    int longestUniqueSubsttr(String str){
+        int i = 0, j = 0, ans = 0;
+        int n = str.length();
+        HashMap<Character, Integer> map = new HashMap<>();
 
+        while (j < n) {
+            map.put(str.charAt(j), map.getOrDefault(str.charAt(j), 0) + 1);
+            if(map.size() == j - i + 1){ // Here k is window size
+                ans = Math.max(ans, j - i + 1);
+            }
+            if(map.size() < j - i + 1){  // If the size of the map is less than the length of the current substring, adjust the window by moving the left pointer i and updating the frequency map
+                while (map.size() < j - i + 1) {
+                map.put(str.charAt(i), map.get(str.charAt(i))- 1);
+                if (map.get(str.charAt(i)) == 0) {
+                    map.remove(str.charAt(i));
+                }
+                i++;
+                }
+            }
+            j++;
+        }
+        return ans;
+    }
+}
