0369157daacb96ee789420f6628bd47931c5aa5d
Kanishka23singh <92868964+Kanishka23singh@users.noreply.github.com>
2024-03-07 00:01:12 +0530

    Create binarySearch.java

---
+++ Binary Search/binarySearch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int binarySearch(int[] nums, int n, int target) {
+        int low = 0;
+        int high = n - 1;
+      
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+               return mid;
+            }
+            else if(nums[mid] < target){
+                low =  mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
