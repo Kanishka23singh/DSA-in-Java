5419a657f9279b76f93fe396454bf06c801c9860
Kanishka23singh <92868964+Kanishka23singh@users.noreply.github.com>
2024-03-20 23:40:14 +0530

    Update 09.Ceil of an element in a Sorted Array.java

--- Binary Search/09.Ceil of an element in a Sorted Array.java
+++ Binary Search/09.Ceil of an element in a Sorted Array.java
@@ -2,22 +2,22 @@ public class CeilInSortedArray {
     public static int findCeil(int[] arr, int target) {
         int left = 0;
         int right = arr.length - 1;
-        int ceilIndex = -1; // Initialize the ceiling index to -1
+        int ceil = -1; // Initialize the ceil to -1
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
             if (arr[mid] == target) {
-                ceilIndex = mid; // Target found, update ceiling index and break
+                ceilIndex =arr[mid]; // Found exact match
                 break;
             } else if (arr[mid] < target) {
                 left = mid + 1; // Search in the right half
             } else {
-                ceilIndex = mid; // Update ceiling index to current mid
+                ceil = arr[mid]; // Update ceil
                 right = mid - 1; // Search in the left half
             }
         }
 
-        return ceilIndex;
+        return ceil;
     }
 }
