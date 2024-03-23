2a8fd55520c15dd9e86cbf108d8b613883e9bfb4
Kanishka23singh <92868964+Kanishka23singh@users.noreply.github.com>
2024-03-24 00:00:15 +0530

    Update Periodic String.java

--- Periodic String.java
+++ Periodic String.java
@@ -1,2 +1,15 @@
 https://www.codingninjas.com/studio/problems/periodic-string_1094904
 https://www.codingninjas.com/studio/problem-details/periodic-string_1094904
+public class SubstringCheck {
+    public static boolean isSubstring(String s, String S) {
+        String T = (s + s).substring(1, s.length() * 2 - 1);  // Create T by concatenating s to itself and removing first and last character
+        return T.indexOf(S) != -1;  // Check if S is a substring of T using indexOf() method
+    }
+
+    public static void main(String[] args) {
+        String s = "hello";
+        String S = "ell";
+        boolean result = isSubstring(s, S);
+        System.out.println(result);  // Output: true
+    }
+}
